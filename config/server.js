0e639b27244471fd40474eafe750962d997f7f16
mlghr <michael.rosenbaum1@gmail.com>
2021-09-16 12:13:27 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '06cd107701e0d115de20283cc2e6b181'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd3d7b7dc-9530-4b13-839f-3e8d60f2d62a'
+};

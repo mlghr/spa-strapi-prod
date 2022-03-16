eb879ac3b778307e4a46f363c535d2f6f3611d57
mlghr <michael.rosenbaum1@gmail.com>
2022-03-15 22:15:58 -0400

    editing middleware

--- config/middleware.js
+++ config/middleware.js
@@ -3,8 +3,11 @@ module.exports = {
     parser: {
       enabled: true,
       multipart: true,
+      formLimit: '10mb', // modify here limit of the form body
+      jsonLimit: '10mb', // modify here limit of the JSON body
+      textLimit: '10mb', // modify here limit of the text body
       formidable: {
-        maxFileSize: 100000 * 1024 * 1024 // Defaults to 200mb (200)
+        maxFileSize: 2000 * 1024 * 1024 // Defaults to 200mb (200)
       }
     }
   },

7892c89e57b37dd4a97f0faa6e9fd633d2e6ee23
mlghr <michael.rosenbaum1@gmail.com>
2022-03-15 22:11:45 -0400

    max filesize increased to 100 MB

--- config/middleware.js
+++ config/middleware.js
@@ -4,7 +4,7 @@ module.exports = {
       enabled: true,
       multipart: true,
       formidable: {
-        maxFileSize: 20000 * 1024 * 1024 // Defaults to 200mb (200)
+        maxFileSize: 100000 * 1024 * 1024 // Defaults to 200mb (200)
       }
     }
   },

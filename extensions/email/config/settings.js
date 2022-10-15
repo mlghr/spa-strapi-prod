9411a3266f53c4a4ce25f164a4b263eaf61055c0
Michael Rosenbaum <mrosenbaum.dev@gmail.com>
2022-10-14 21:30:03 -0400

    remove line about email provider

--- extensions/email/config/settings.js
+++ extensions/email/config/settings.js
@@ -1,12 +1,12 @@
-'use strict'
+// 'use strict'
 
-module.exports = {
-  provider: 'sendgrid',
-  providerOptions: {
-    apiKey: process.env.SENDGRID_API_KEY,
-  },
-  settings: {
-    defaultFrom: 'lsouthland@spa-food.org',
-    defaultTo: 'no-reply@spa-food.org'
-  },
-}
+// module.exports = {
+//   provider: 'sendgrid',
+//   providerOptions: {
+//     apiKey: process.env.SENDGRID_API_KEY,
+//   },
+//   settings: {
+//     defaultFrom: 'lsouthland@spa-food.org',
+//     defaultTo: 'no-reply@spa-food.org'
+//   },
+// }

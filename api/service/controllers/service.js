e969155e224a2336da100db6d9eb5ef8d21d5da7
Michael Rosenbaum <mrosenbaum.dev@gmail.com>
2022-10-26 22:06:52 -0400

    controllers and routers

--- api/service/controllers/service.js
+++ api/service/controllers/service.js
@@ -1,8 +1,5 @@
-'use strict';
+// path: ./src/api/<content-type-name>/controllers/<controller-name>.js
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = {};
+module.exports = createCoreController("api::service.service");
